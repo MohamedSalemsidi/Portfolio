e99286973d919e300043a5e24a31066dd0622fe6
MohamedSalemsidi <modestsalem2017@gmail.com>
2023-02-18 18:20:10 +0700

    Upload the right files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu");
+const action = document.getElementById("action");
+
+ 
+function hundelMenu() {
+    alert("Salam");
+    menu.classList.toggle("is-active");
+    action.classList.toggle("is-active");
+};
